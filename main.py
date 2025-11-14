db92d414850ed7dd6cda0b2804992d728e06b608
8044734 <maryamimrank210@gmail.com>
2025-11-14 01:01:54 +0000

    Coding assessment

--- main.py
+++ main.py
@@ -4,14 +4,27 @@ from datetime import datetime, timedelta
 # -------- Level 1 --------
 # TODO: Create a function to view all books that are currently available
 # Output should include book ID, title, and author
-def view_Books():
-    
+def available_Books(library_books: list[dict]) -> list[dict]:
+    available_Books = []
+    for book in library_books:
+      if book["available"] == True:
+       "id": book["id"]
+      "title": book["title"]
+      "author": book["author"]
+
+    return[available_Books]
+# This code tells about the function of books that are available
 
 # -------- Level 2 --------
 # TODO: Create a function to search books by author OR genre
 # Search should be case-insensitive
 # Return a list of matching books
- def search_Books():
+def search_Books(library_Books: list[dict]) -> list[dict]:
+    search_Books = []
+    author.lower
+    genre.lower
+    return[search_Books]
+# This code tells about the function of searching books by author or genre
 
 # -------- Level 3 --------
 # TODO: Create a function to checkout a book by ID
@@ -21,12 +34,11 @@ def view_Books():
 #   - Increment the checkouts counter
 # If it is not available:
 #   - Print a message saying it's already checked out
-  def checkout_Book():
+    
 
 # -------- Level 4 --------
 # TODO: Create a function to return a book by ID
 # Set its availability to True and clear the due_date
-   def return_Book():
 # TODO: Create a function to list all overdue books
 # A book is overdue if its due_date is before today AND it is still checked out
   
